cee6b9acd706ff79bd826d025ec6d98f0e74bcd1
Kimberly <ksereduck@mozilla.com>
2018-04-03 09:09:08 -0600

    added accessibility tests

--- test/integration/accessibility/test_accessibility.py
+++ test/integration/accessibility/test_accessibility.py
@@ -7,13 +7,13 @@ from pytest_axe.pytest_axe import run_axe
 @pytest.mark.accessibility
 def test_login_page_accessibility(login_page):
     """Test login page for accessibility violations."""
-    run_axe(login_page)
+    run_axe(login_page, None, None, 'critical')
 
 
 @pytest.mark.accessibility
 def test_home_page_guest_accessibility(home_page):
     """Test home page as guest for accessibility violations."""
-    run_axe(home_page)
+    run_axe(home_page, None, None, 'critical')
 
 
 @pytest.mark.accessibility
@@ -21,4 +21,4 @@ def test_home_page_logged_in_accessibility(fxa_account, login_page):
     """Test home page when logged in for accessibility violations."""
     fxa = fxa_account
     home_page = login_page.sign_in(fxa.email, fxa.password)
-    run_axe(home_page)
+    run_axe(home_page, None, None, 'critical')
