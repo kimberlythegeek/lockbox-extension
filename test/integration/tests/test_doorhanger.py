2d5249f87ffbb91225b66d42282654f39d22edbe
Kimberly <ksereduck@mozilla.com>
2018-04-05 10:54:50 -0600

    updated doorhanger test

--- test/integration/tests/test_doorhanger.py
+++ test/integration/tests/test_doorhanger.py
@@ -6,16 +6,8 @@ def test_door_hanger_interaction(fxa_account, login_page):
     fxa = fxa_account
     home_page = login_page.sign_in(fxa.email, fxa.password)
     entry = home_page.create_empty_entry()
-    #
-    # entry.set_site_name('Tuna, Inc.')
-    # entry.set_site_url('https://satuna.org')
-    # entry.set_username('tuna4life')
-    # entry.set_password('tunafish')
-    # entry.set_note('The tuna swim at midnight')
-    # entry.save()
-    # sleep(5)
+
     lists = home_page.door_hanger.find_entrys()
-    print(lists)
-    assert 'Tuna, Inc.' in lists[0].title
+    assert '(no site name)' in lists[0].title
     entry = lists[0].click()
     assert 'Tuna, Inc.' in entry.title
